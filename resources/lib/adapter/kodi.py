f8df0be68256cf03a4c170e77e300b6c8bb169b2
SansanLaTulipe <dpelliss@gmail.com>
2022-04-18 23:32:09 +0200

    Add repository to handle kodi episodes

--- resources/lib/adapter/kodi.py
+++ resources/lib/adapter/kodi.py
@@ -2,6 +2,10 @@ class MovieRepository:
     def __init__(self, jsonrpc):
         self.jsonrpc = jsonrpc
 
+    @staticmethod
+    def getKind():
+        return 'movie'
+
     def retrieveById(self, movieId):
         response = self.jsonrpc.call(
             'VideoLibrary.GetMovieDetails',
@@ -37,4 +41,50 @@ class MovieRepository:
             'id': movie.get('movieid'),
             'tmdbId': movie.get('uniqueid').get('tmdb'),
             'isWatched': movie.get('playcount') > 0
+        }
+
+
+class EpisodeRepository:
+    def __init__(self, jsonrpc):
+        self.jsonrpc = jsonrpc
+
+    @staticmethod
+    def getKind():
+        return 'episode'
+
+    def retrieveById(self, episodeId):
+        response = self.jsonrpc.call(
+            'VideoLibrary.GetEpisodeDetails',
+            {'episodeid': episodeId},
+            ['uniqueid', 'playcount']
+        )
+        return self._buildEntity(response.get('result', {}).get('episodedetails'))
+
+    def retrieveAll(self):
+        response = self.jsonrpc.call(
+            'VideoLibrary.GetEpisodes',
+            {},
+            ['uniqueid', 'playcount']
+        )
+        return list(map(
+            self._buildEntity,
+            response.get('result').get('episodes')
+        ))
+
+    def updateWatchedStatus(self, episodeId, isWatched):
+        self.jsonrpc.call(
+            'VideoLibrary.SetEpisodeDetails',
+            {
+                'episodeid': episodeId,
+                'playcount': 1 if isWatched else 0
+            }
+        )
+
+    def _buildEntity(self, episode):
+        if not episode:
+            return None
+        return {
+            'id': episode.get('episodeid'),
+            'tmdbId': episode.get('uniqueid').get('tvdb'),
+            'isWatched': episode.get('playcount') > 0
         }
--- resources/test/adapter/test_kodi.py
+++ resources/test/adapter/test_kodi.py
@@ -1,16 +1,16 @@
 from resources.test.testmod import unittest
 from resources.test.testmod import mock
-from resources.lib.adapter.kodi import MovieRepository
+from resources.lib.adapter import kodi
 
 
 class MovieRepositoryShould(unittest.TestCase):
     @mock.patch('resources.lib.infra.kodi.JsonRPC')
     def setUp(self, jsonrpc):
         self.jsonrpc = jsonrpc
-        self.repo = MovieRepository(self.jsonrpc)
+        self.repo = kodi.MovieRepository(self.jsonrpc)
 
     def test_build_movie_when_retrieving_with_known_id(self):
-        fakeResponse = {'result': {'moviedetails': self._buildKodiMovieObject(1)}}
+        fakeResponse = {'result': {'moviedetails': self._buildMovieObject(1)}}
         self.jsonrpc.call = mock.Mock(return_value=fakeResponse)
 
         movie = self.repo.retrieveById(5)
@@ -39,8 +39,8 @@ class MovieRepositoryShould(unittest.TestCase):
 
     def test_build_movies_when_retrieving_all(self):
         fakeResponse = {'result': {'movies': [
-            self._buildKodiMovieObject(1),
-            self._buildKodiMovieObject(0)
+            self._buildMovieObject(1),
+            self._buildMovieObject(0)
         ]}}
         self.jsonrpc.call = mock.Mock(return_value=fakeResponse)
 
@@ -77,7 +77,7 @@ class MovieRepositoryShould(unittest.TestCase):
         })
 
     @staticmethod
-    def _buildKodiMovieObject(playcount):
+    def _buildMovieObject(playcount):
         return {
             'movieid': 5,
             'uniqueid': {
@@ -88,6 +88,98 @@ class MovieRepositoryShould(unittest.TestCase):
 
     @staticmethod
     def _buildMovieEntity(isWatched):
+        return {
+            'id': 5,
+            'tmdbId': 1005,
+            'isWatched': isWatched
+        }
+
+
+class EpisodeRepositoryShould(unittest.TestCase):
+    @mock.patch('resources.lib.infra.kodi.JsonRPC')
+    def setUp(self, jsonrpc):
+        self.jsonrpc = jsonrpc
+        self.repo = kodi.EpisodeRepository(self.jsonrpc)
+
+    def test_build_episode_when_retrieving_with_known_id(self):
+        fakeResponse = {'result': {'episodedetails': self._buildEpisodeObject(1)}}
+        self.jsonrpc.call = mock.Mock(return_value=fakeResponse)
+
+        episode = self.repo.retrieveById(5)
+
+        self.jsonrpc.call.assert_called_once_with(
+            'VideoLibrary.GetEpisodeDetails',
+            {'episodeid': 5},
+            ['uniqueid', 'playcount']
+        )
+        self.assertEqual(
+            self._buildEpisodeEntity(True),
+            episode
+        )
+
+    def test_return_none_when_retrieving_with_unknown_id(self):
+        self.jsonrpc.call = mock.Mock(return_value={})
+
+        episode = self.repo.retrieveById('unknown_id')
+
+        self.jsonrpc.call.assert_called_once_with(
+            'VideoLibrary.GetEpisodeDetails',
+            {'episodeid': 'unknown_id'},
+            ['uniqueid', 'playcount']
+        )
+        self.assertIsNone(episode)
+
+    def test_build_episodes_when_retrieving_all(self):
+        fakeResponse = {'result': {'episodes': [
+            self._buildEpisodeObject(1),
+            self._buildEpisodeObject(0)
+        ]}}
+        self.jsonrpc.call = mock.Mock(return_value=fakeResponse)
+
+        episodes = self.repo.retrieveAll()
+
+        self.jsonrpc.call.assert_called_once_with(
+            'VideoLibrary.GetEpisodes',
+            {},
+            ['uniqueid', 'playcount']
+        )
+        self.assertEqual(
+            [self._buildEpisodeEntity(True), self._buildEpisodeEntity(False)],
+            episodes
+        )
+
+    def test_change_episode_playcount_to_0_when_updating_to_unwatched(self):
+        self.jsonrpc.call = mock.Mock()
+
+        self.repo.updateWatchedStatus(5, False)
+
+        self.jsonrpc.call.assert_called_once_with('VideoLibrary.SetEpisodeDetails', {
+            'episodeid': 5,
+            'playcount': 0
+        })
+
+    def test_change_episode_playcount_to_1_when_updating_to_watched(self):
+        self.jsonrpc.call = mock.Mock()
+
+        self.repo.updateWatchedStatus(5, True)
+
+        self.jsonrpc.call.assert_called_once_with('VideoLibrary.SetEpisodeDetails', {
+            'episodeid': 5,
+            'playcount': 1
+        })
+
+    @staticmethod
+    def _buildEpisodeObject(playcount):
+        return {
+            'episodeid': 5,
+            'uniqueid': {
+                'tvdb': 1005
+            },
+            'playcount': playcount
+        }
+
+    @staticmethod
+    def _buildEpisodeEntity(isWatched):
         return {
             'id': 5,
             'tmdbId': 1005,
