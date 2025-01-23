93e5d83e455f5db45f7e76ffb48ba5def3edc519
SansanLaTulipe <dpelliss@gmail.com>
2025-01-23 14:22:52 +0100

    Refactor service tests to mock infra's classes instead of adapter's

--- resources/test/features/environment.py
+++ resources/test/features/environment.py
@@ -1,3 +1,4 @@
+from configparser import ConfigParser
 from unittest import mock
 
 from behave.model import Scenario
@@ -6,23 +7,24 @@ from xbmcaddon import Addon
 
 from resources.lib.adapter import CacheRepository
 from resources.lib.adapter import Logger
-from resources.lib.adapter.betaseries import BearerRepository
-from resources.lib.adapter.betaseries import EpisodeRepository as BsEpisodeRepository
-from resources.lib.adapter.betaseries import MovieRepository as BsMovieRepository
-from resources.lib.adapter.kodi import EpisodeRepository as KodiEpisodeRepository
-from resources.lib.adapter.kodi import MovieRepository as KodiMovieRepository
+from resources.lib.adapter import Settings
+from resources.lib.infra import SimpleCache
+from resources.lib.infra.betaseries import Http as BsHttp
+from resources.lib.infra.kodi import JsonRPC as KodiJsonRPC
 from resources.lib.util import Container
 
 
 class MockContainer(Container):
     @mock.patch('xbmcaddon.Addon')
-    def _initAddon(self, addon: Addon) -> Addon:
-        addon().getAddonInfo.side_effect = lambda *args, **kwargs: {
+    def _initAddon(self, addonMock: Addon) -> Addon:
+        addon = addonMock()
+
+        addon.getAddonInfo.side_effect = lambda *args, **kwargs: {
             'id': 'addon.name',
             'name': 'Addon name',
             'path': '.'
         }.get(args[0])
-        addon().getSetting.side_effect = lambda *args, **kwargs: {
+        addon.getSetting.side_effect = lambda *args, **kwargs: {
             'sync_movies': 'true',
             'sync_episodes': 'true',
             'notify_mail': 'false',
@@ -30,35 +32,40 @@ class MockContainer(Container):
             'update_profile': 'false'
         }.get(args[0])
 
-        return addon()
+        return addon
 
-    @mock.patch('resources.lib.adapter.betaseries.BearerRepository')
-    def _initBetaseriesBearerRepository(self, repo: BearerRepository) -> BearerRepository:
-        return repo
+    @mock.patch('resources.lib.infra.betaseries.Http')
+    def _initBetaseriesHttp(self, httpMock: BsHttp) -> BsHttp:
+        return httpMock()
 
-    @mock.patch('resources.lib.adapter.betaseries.EpisodeRepository')
-    def _initBetaseriesEpisodeRepository(self, repo: BsEpisodeRepository) -> BsEpisodeRepository:
-        return repo
+    @mock.patch('configparser.ConfigParser')
+    def _initCacheRepository(self, cacheMock: SimpleCache) -> CacheRepository:
+        cache = cacheMock()
+        cache.get.side_effect = lambda key: {
+            'addon.name.betaseries.token': 'bearer',
+            'addon.name.betaseries.movie.endpoint': 1,
+            'addon.name.betaseries.episode.endpoint': 2,
+        }.get(key)
 
-    @mock.patch('resources.lib.adapter.betaseries.MovieRepository')
-    def _initBetaseriesMovieRepository(self, repo: BsMovieRepository) -> BsMovieRepository:
-        return repo
+        return CacheRepository(
+            cache,
+            self.get('settings').getAddonId()
+        )
 
-    @mock.patch('resources.lib.adapter.CacheRepository')
-    def _initCacheRepository(self, repo: CacheRepository) -> CacheRepository:
-        return repo
-
-    @mock.patch('resources.lib.adapter.kodi.EpisodeRepository')
-    def _initKodiEpisodeRepository(self, repo: KodiEpisodeRepository) -> KodiEpisodeRepository:
-        return repo
-
-    @mock.patch('resources.lib.adapter.kodi.MovieRepository')
-    def _initKodiMovieRepository(self, repo: KodiMovieRepository) -> KodiMovieRepository:
-        return repo
+    @mock.patch('resources.lib.infra.kodi.JsonRPC')
+    def _initKodiJsonrpc(self, jsonMock: KodiJsonRPC) -> KodiJsonRPC:
+        return jsonMock()
 
     @mock.patch('resources.lib.adapter.Logger')
-    def _initLogger(self, logger: Logger) -> Logger:
-        return logger
+    def _initLogger(self, loggerMock: Logger) -> Logger:
+        return loggerMock()
+
+    @mock.patch('configparser.ConfigParser')
+    def _initSettings(self, configMock: ConfigParser) -> Settings:
+        return Settings(
+            self.get('addon'),
+            configMock()
+        )
 
 
 def before_scenario(context: Context, scenario: Scenario) -> None:
--- resources/test/features/steps/step_sync.py
+++ resources/test/features/steps/step_sync.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from behave import given
 from behave import then
@@ -48,13 +49,14 @@ def step_given_a_medium_exists_in_repo(context: Context, mediumType: str, repoTy
 
 @when('this {mediumType} triggers a Kodi notification')
 def step_when_kodi_notification_is_triggered(context: Context, mediumType: str) -> None:
-    bearerRepoMock = context.dependencyInjector.get('betaseries.bearer.repository')
-    kodiRepoMock = context.dependencyInjector.get(f'kodi.{mediumType}.repository')
-    bsRepoMock = context.dependencyInjector.get(f'betaseries.{mediumType}.repository')
-
-    bearerRepoMock.isActive.return_value = True
-    kodiRepoMock.retrieveById.return_value = _retrieveMediumFromRepo(context, 'Kodi')
-    bsRepoMock.retrieveByUniqueId.return_value = _retrieveMediumFromRepo(context, 'BetaSeries')
+    context.dependencyInjector.get('betaseries.http').get.return_value = _buildBsMediumFromEntity(
+        mediumType,
+        _retrieveMediumFromRepo(context, 'BetaSeries')
+    )
+    context.dependencyInjector.get('kodi.jsonrpc').call.return_value = _buildKodiMediumFromEntity(
+        mediumType,
+        _retrieveMediumFromRepo(context, 'Kodi')
+    )
 
     context.dependencyInjector.get('daemon.sync').onNotification(
         None,
@@ -68,32 +70,191 @@ def step_when_kodi_notification_is_triggered(context: Context, mediumType: str) 
 
 @when('the complementary scan runs to synchonize {mediumType}s')
 def step_when_run_complementary_scan_sync(context: Context, mediumType: str) -> None:
-    cacheRepoMock = context.dependencyInjector.get('cache.repository')
-    bearerRepoMock = context.dependencyInjector.get('betaseries.bearer.repository')
-    kodiRepoMock = context.dependencyInjector.get(f'kodi.{mediumType}.repository')
-    bsRepoMock = context.dependencyInjector.get(f'betaseries.{mediumType}.repository')
+    context.dependencyInjector.get('betaseries.http').get.side_effect = [
+        {
+            'member': {
+                'id': 1
+            }
+        },
+        {
+            'events': [
+                {
+                    'ref_id': 3,
+                    'id': _retrieveMediumFromRepo(context, 'BetaSeries').id
+                }
+            ]
+        },
+        _buildBsMediumFromEntity(
+            mediumType,
+            _retrieveMediumFromRepo(context, 'BetaSeries')
+        )
+    ]
+    context.dependencyInjector.get('kodi.jsonrpc').call.return_value = _buildKodiMediumFromEntity(
+        mediumType,
+        _retrieveMediumFromRepo(context, 'Kodi'),
+        isList=True
+    )
 
-    cacheRepoMock.getBetaseriesEndpoint.return_value = 'endpoint-id'
-    bearerRepoMock.isActive.return_value = True
-    kodiRepoMock.retrieveAll.return_value = [_retrieveMediumFromRepo(context, 'Kodi')]
-    bsRepoMock.retrieveById.return_value = _retrieveMediumFromRepo(context, 'BetaSeries')
-    bsRepoMock.retrieveUpdatedIdsFrom.return_value = [{}]
 
     context.dependencyInjector.get(f'{mediumType}.sync').synchronize()
 
 
-@then('this {mediumType} should be marked as "{watchedMark}" on {repoType}')
-def step_then_assert_medium_should_have_watched_mark(
+@then('this episode should be marked as "{watchedMark}" on BetaSeries')
+def step_then_assert_episode_should_have_watched_mark_on_betaseries(
     context: Context,
-    mediumType: str,
-    watchedMark: str,
-    repoType: str
+    watchedMark: str
 ) -> None:
     expectedIsWatched = _buildIsWatch(watchedMark)
-    actualIsWatched = _retrieveMediumFromRepo(context, repoType).isWatched
+    actualEpisode = _retrieveMediumFromRepo(context, 'BetaSeries')
 
-    assert expectedIsWatched == actualIsWatched, \
+    if expectedIsWatched:
+        context.dependencyInjector.get('betaseries.http').post.assert_any_call(
+            '/episodes/watched',
+            {'id': actualEpisode.id}
+        ), \
+            'The episode should be marked as {}, but it is not'.format(watchedMark)
+    else:
+        context.dependencyInjector.get('betaseries.http').delete.assert_any_call(
+            '/episodes/watched',
+            {'id': actualEpisode.id}
+        ), \
+            'The episode should be marked as {}, but it is not'.format(watchedMark)
+
+
+@then('this movie should be marked as "{watchedMark}" on BetaSeries')
+def step_then_assert_movie_should_have_watched_mark_on_betaseries(
+    context: Context,
+    watchedMark: str
+) -> None:
+    expectedIsWatched = _buildIsWatch(watchedMark)
+    actualMovie = _retrieveMediumFromRepo(context, 'BetaSeries')
+
+    context.dependencyInjector.get('betaseries.http').post.assert_any_call(
+        '/movies/movie',
+        {
+            'id': actualMovie.id,
+            'state': 1 if expectedIsWatched else 0,
+            'mail': 0,
+            'twitter': 0,
+            'profile': 0
+        }
+    ), \
+        'The movie should be marked as {}, but it is not'.format(watchedMark)
+
+
+@then('this {mediumType} should be marked as "{watchedMark}" on Kodi')
+def step_then_assert_medium_should_have_watched_mark_on_kodi(
+    context: Context,
+    mediumType: str,
+    watchedMark: str
+) -> None:
+    expectedIsWatched = _buildIsWatch(watchedMark)
+    expectedMethod = 'VideoLibrary.Set{}Details'.format(mediumType.capitalize())
+    expectedMediumIdName = '{}id'.format(mediumType)
+    actualMedium = _retrieveMediumFromRepo(context, 'Kodi')
+
+    context.dependencyInjector.get('kodi.jsonrpc').call.assert_any_call(
+        expectedMethod,
+        {
+            expectedMediumIdName: actualMedium.id,
+            'playcount': 1 if expectedIsWatched else 0
+        }
+    ), \
         'The {} should be marked as {}, but it is not'.format(mediumType, watchedMark)
+
+
+def _buildBsMediumFromEntity(type: str, medium: MediumEntity) -> dict:
+    if 'episode' == type:
+        return _buildBsEpisodeFromEntity(medium)
+    else:
+        return _buildBsMovieFromEntity(medium)
+
+
+def _buildBsEpisodeFromEntity(medium: MediumEntity) -> dict:
+    titleSplit = re.findall(r"(.+) S([0-9]{2})E([0-9]{2})", medium.title)[0]
+
+    return {
+        'episode': {
+            'id': medium.id,
+            'thetvdb_id': medium.uniqueId,
+            'show': {
+                'title': titleSplit[0]
+            },
+            'season': int(titleSplit[1]),
+            'episode': int(titleSplit[2]),
+            'user': {
+                'seen': medium.isWatched
+            }
+        }
+    }
+
+
+def _buildBsMovieFromEntity(medium: MediumEntity) -> dict:
+    return {
+        'movie': {
+            'id': medium.id,
+            'tmdb_id': medium.uniqueId,
+            'title': medium.title,
+            'user': {
+                'status': 1 if medium.isWatched else 0
+            }
+        }
+    }
+
+
+def _buildKodiMediumFromEntity(type: str, medium: MediumEntity, isList: bool = False) -> dict:
+    if 'episode' == type:
+        return _buildKodiEpisodeFromEntity(medium, isList)
+    else:
+        return _buildKodiMovieFromEntity(medium, isList)
+
+
+def _buildKodiEpisodeFromEntity(medium: MediumEntity, isList: bool) -> dict:
+    episode = {
+        'episodeid': medium.id,
+        'uniqueid': {
+            'tvdb': medium.uniqueId
+        },
+        'sorttitle': medium.title,
+        'playcount': 1 if medium.isWatched else 0
+    }
+
+    if isList:
+        return {
+            'result': {
+                'episodes': [episode]
+            }
+        }
+    else:
+        return {
+            'result': {
+                'episodedetails': episode
+            }
+        }
+
+
+def _buildKodiMovieFromEntity(medium: MediumEntity, isList: bool) -> dict:
+    movie = {
+        'movieid': medium.id,
+        'uniqueid': {
+            'tmdb': medium.uniqueId
+        },
+        'sorttitle': medium.title,
+        'playcount': 1 if medium.isWatched else 0
+    }
+
+    if isList:
+        return {
+            'result': {
+                'movies': [movie]
+            }
+        }
+    else:
+        return {
+            'result': {
+                'moviedetails': movie
+            }
+        }
 
 
 def _buildMedium(title: str, isWatched: bool) -> MediumEntity:
